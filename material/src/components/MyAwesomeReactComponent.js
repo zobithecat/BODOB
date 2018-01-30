11e6d464e5522e2b4415201068b6df87b042d747
DESKTOP-RO3F8N8\meal <alfl8514@naver.com>
2018-01-30 22:33:33 +0900

    1일차 2018/1/30

---
+++ material/src/components/MyAwesomeReactComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+ 
+const MyAwesomeReactComponent = () => (
+<div>
+  <RaisedButton label="Default" />
+  <RaisedButton label="Default2" />
+ </div>
+);
+ 
+export default MyAwesomeReactComponent;
